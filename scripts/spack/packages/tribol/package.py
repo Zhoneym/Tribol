1d7a6db816d7f4a1cd8faaef22fbfc2bfa375cb6
EB Chin <chin23@llnl.gov>
2024-10-28 17:14:52 -0700

    update spack recipes

--- scripts/spack/packages/tribol/package.py
+++ scripts/spack/packages/tribol/package.py
@@ -234,7 +234,7 @@ class Tribol(CachedCMakePackage, CudaPackage, ROCmPackage):
 
             entries.append(cmake_cache_option("ENABLE_HIP", True))
 
-            rocm_root = os.path.dirname(spec["hip"].prefix)
+            rocm_root = spec["hip"].prefix
             entries.append(cmake_cache_path("ROCM_PATH", rocm_root))
 
             hip_link_flags = ""
